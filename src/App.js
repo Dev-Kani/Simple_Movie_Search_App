2a2e1b5c3cc7723452091ea0fc089c30a130f2aa
Kanishka Samarathunga <kanishkakcs@gmail.com>
2022-05-04 15:42:29 +0530

    useState modified

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import SearchBox from "./components/SearchBox";
 const App = () => {
 
   const [movies, setMovies] = useState([])
-  const [searchValue, setSearchValue] = useState('Avatar')
+  const [searchValue, setSearchValue] = useState('')
 
   const getMovieRequest = async (searchValue) => {
     const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=6d394752`
